f3418fca2816ec0b1eaa3837c3a10eaa391a95e3
Ricky Dane Perlick <danericky@outlook.de>
2025-03-08 19:59:14 +0100

    playing around some more

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,36 @@ use std::fs;
 
 #[tokio::main]
 async fn main() {
+
+    loop {
+        // Get user input
+        let mut user_input = String::new();
+        std::io::stdin()
+            .read_line(&mut user_input)
+            .expect("Couldn't read user_input from stdin");
+
+        let output = get_pred_files("/Users/rickyperlick/Documents", &user_input).await;
+        println!("{}", output);
+    }
+}
+
+async fn get_pred_files(path: &str, query: &str) -> String {
+    // Load the system prompt so the llm knows what it needs to do
     let system_prompt =
         fs::read_to_string("./system_prompt.txt").expect("Couldn't read system prompt file");
 
+    println!("{}", system_prompt);
+
+    // Load the specific model to do the inference with
+    let model = Llama::new_chat().await.unwrap();
+
+    let mut chat = model.chat().with_system_prompt(&system_prompt);
+
     let mut ls_entries: String = String::from("<list>\n");
 
-    let entries = fs::read_dir(".");
+    let entries = fs::read_dir(path);
     if entries.is_err() {
-        return;
+        return "-".into();
     }
 
     for entry in entries.unwrap() {
@@ -22,20 +44,14 @@ async fn main() {
 
     ls_entries.push_str("</list>\n");
 
-    println!("Wonach wird gesucht? > ");
+    // Append the user input
+    ls_entries.push_str(&format!("<input>\n{}</input>", query));
 
-    let mut user_input: String = String::new();
-    std::io::stdin().read_line(&mut user_input).expect("Couldn't read user_input from stdin");
-    ls_entries.push_str(&format!("<input>\n{}</input>", user_input));
-
+    // Print whole input
     println!("{}", ls_entries);
 
-    let model = Llama::builder()
-            .with_source(LlamaSource::llama_3_2_3b_chat())
-            .build()
-            .await
-            .unwrap();
-    let mut chat = model.chat().with_system_prompt(system_prompt);
+    // Start prediction
+    let output = chat(&ls_entries).all_text().await;
 
-    chat(&ls_entries).to_std_out().await.expect("chat didn't go as planned");
+    return output;
 }
